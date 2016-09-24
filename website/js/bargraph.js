ed13d5b3bcb6b9349d160dd6f1f1367af96ed7f5
Foris Kuang <forisk@uw.edu>
2016-09-24 00:20:27 -0700

    bargraph.js added, need to work out how to get require to work with it, may be a common.js/node.js issue. If this works need to pass data in and then everything should theoretically work out.

---
+++ website/js/bargraph.js
@@ -0,0 +1,82 @@
+"use strict";
+console.log("Hello");
+
+define(function (require) {
+	var React = require('react');
+	var ReactDOM = require('react-dom');
+	var BarChart = require('react-d3-basic').BarChart;
+});
+
+var BarGraph = React.createClass({
+	getInitialState: function() {
+		return {data: []}
+	},
+	render: function() {
+        return ( 
+        	<div>
+            <
+            BarChart width = {this.state.width}
+            height = {this.state.height}
+            data = {this.state.data}
+            chartSeries = {this.state.series}
+            x = {x}
+            xScale = {xScale}
+            y = {y}
+            yTicks = {yTicks}
+            showYGrid = {true}
+            showLegend = {false}
+            /> 
+            </div>
+        )
+    }
+});
+
+var width = 600,
+	height = 600,
+	title = "Probability of Drug",
+	chartSeries = [
+		{
+			field: 'value',
+			name: 'Value'
+		}
+	],
+	xScale = 'ordinal',
+	x = function(d) {
+		return d.drug;
+	},
+	xLabel = "Drug",
+	yLabel = "EEG Value",
+	yTicks = [5, "%"];
+
+var BarGraphDummyTest = React.createClass({
+	
+	render: function() {
+		return (
+			<div>
+			<
+			BarChart width = {width}
+			title = {title}
+            height = {height}
+            data = {this.props.data}
+            chartSeries = {chartSeries}
+            x = {x}
+            xScale = {xScale}
+            yTicks = {yTicks}
+            showYGrid = {true}
+            showLegend = {false}
+            /> 
+            </div>
+		)
+	}
+});
+
+ReactDOM.render(
+	console.log("rendeirng"),
+	<BarGraphDummyTest data={data}/>,
+	document.getElementById('data_bar')
+);
+
+// ReactDOM.render(
+//	<BarGraphDummyTest url="/api/data"/>,
+//	document.getElementById('data_bar')
+// );
---
+++ website/js/bargraphdummydata.js
@@ -0,0 +1,14 @@
+var data = [
+	{
+		drug: "Caffeine",
+		value: 55
+	},
+	{
+		drug: "Alcohol",
+		value: 24
+	},
+	{
+		drug: "Cocaine",
+		value: 95
+	}
+];
